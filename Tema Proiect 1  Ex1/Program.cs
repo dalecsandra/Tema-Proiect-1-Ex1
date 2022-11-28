6aca843151b3c1da0e0b2e1ae574693571a5cf07
dascaleacalexandra <dascaleacalexandra@outlook.com>
2022-11-28 20:41:05 +0200

    Add project files.

---
+++ Tema Proiect 1  Ex1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Tema_Proiect_1__Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Ex1
+               Scrieti un program care citind de la tastaura cele trei dimensiuni ale unui
+               paralelipiped dreptunghic, va afisa volumul lui*/
+
+            int latime = 10;
+            int lungime = 20;
+            int inaltime = 15;
+
+            Console.WriteLine("Acest program calculeaza volumul unui paralelipiped dreptunghic");
+
+            Console.WriteLine("Introduceti latimea");
+            latime = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduceti lungimea");
+            lungime = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduceti inaltimea");
+            inaltime = int.Parse(Console.ReadLine());
+
+            int volumulParalelipipeduluiDreptunghic = (latime*lungime*inaltime);
+            Console.WriteLine(volumulParalelipipeduluiDreptunghic);
+        }
+    }
+}
